62638eaae2a7992900647c1a06e6a4c215162d93
norman-andrians <norman25.na@gmail.com>
2022-03-09 21:50:36 +0700

    patch 0.41 revisi-3

--- data/audio-data.js
+++ data/audio-data.js
@@ -1,10 +1,14 @@
 const audioData = {
     'nama' : [
         'bel sekolah',
-        'suasana sekolah'
+        'suasana sekolah',
+        'hutan prenjon',
+        'ambien seram'
     ],
     'source' : [
         './audio/sound_000_bel-sekolah.mp3',
-        './audio/sound_001_suasana-sekolah.mp3'
+        './audio/sound_001_suasana-sekolah.mp3',
+        './audio/sound_002_hutan-malam.mp3',
+        './audio/sound_003_suasana-seram-(mentahan).mp3'
     ]
 }
